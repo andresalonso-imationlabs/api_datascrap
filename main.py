ff68214bf09e54c3a7dd21e76f8982c924a316ec
andresalonso-imationlabs <andres.alonso@imationlabs.com>
2022-11-06 20:49:16 +0100

    firstPush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from lxml import html
+import requests
+import json
 
 app = FastAPI()
 
@@ -7,9 +10,23 @@ app = FastAPI()
 async def root():
     return {"Welcome": "you"}
 
-@app.get("/scrap")
-async def root():
-    return {"asdf": "sadf"}
+@app.get("/scrap/{url}/{key_element}/{key_find_by}/{key_id}")
+def read_root(url: str, key_element: str, key_find_by: str, key_id: str, request: Request):
+     page = requests.get("http://WWW."+url)
+     tree = html.fromstring(page.content)
+     #return tree
+
+     rows = tree.xpath("//"+key_element+"[contains(@"+key_find_by+", '"+key_id+"')]//text()")
+     return rows
+
+'''
+async def scrapXpathkey(url, key):
+        page = requests.get(url)
+        tree = html.fromstring(page.content)        
+        rows = tree.xpath(key)
+        return rows
+'''
+
 
 if __name__ == "__main__":
     uvicorn.run(
